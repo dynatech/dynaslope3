496e59597c11fb53c078b404a15a7f267fea1cc8
jhndvdgvr <david063095@gmail.com>
2019-02-21 13:41:12 +0800

    remove comments

--- run.py
+++ run.py
@@ -8,8 +8,6 @@ from connection import create_app, SOCKETIO
 
 CONFIG_NAME = os.getenv("FLASK_CONFIG")
 APP = create_app(CONFIG_NAME)
-# SOCKETIO = SocketIO(APP)
 
 if __name__ == "__main__":
-    # APP.run(debug=True)
     SOCKETIO.run(APP, host='localhost', port=5000, debug=True)
--- src/api/inbox_controller.py
+++ src/api/inbox_controller.py
@@ -33,19 +33,11 @@ def get_unregistered_inbox(is_api = 1):
     # /my-route?page=10&filter=test   -> page: 10  filter: 'test'
     # /my-route?page=10&filter=10     -> page: 10  filter: '10'
 
-    # member = Membership.query.first()
-    # membership_schema = MembershipSchema()
-    # output = membership_schema.dump(member).data
-
-    # unregisted_inbox = SmsInboxUsers.query.limit(10).all()
     unregisted_inbox = DB.session.query(SmsInboxUsers, UserMobile, Users).select_from(SmsInboxUsers).join(UserMobile,SmsInboxUsers.mobile_id == UserMobile.mobile_id).join(Users,UserMobile.user_id == Users.user_id).limit(10).all()
     final_data = []
     for inbox, mobile, user in unregisted_inbox:
         final_data.append({"mobile_id:" : inbox.mobile_id, "mobile_number" : mobile.sim_num, "firstname":user.firstname, "message": inbox.sms_msg})
-    
-    # print(final_data)
-    # schema = SmsInboxUsersSchema(many=True)
-    # output = schema.dump(unregisted_inbox).data
+
     return_data = final_data
     if is_api == 1:
         return_data = jsonify(final_data)
