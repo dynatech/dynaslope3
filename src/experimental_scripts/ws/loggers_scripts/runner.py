735e372fb6545d8e38eade61e83bc040358f3138
John Geliberte <gelibertejohn@gmail.com>
2019-03-07 08:44:54 +0800

    + Added script for handling data logger sms

--- src/experimental_scripts/ws/loggers_scripts/runner.py
+++ src/experimental_scripts/ws/loggers_scripts/runner.py
@@ -9,8 +9,6 @@ import memcache
 import argparse
 import gsm_modules as modem
 import db_lib as dbLib
-from pprint import pprint
-
 
 class GsmServer:
 
@@ -39,8 +37,8 @@ class GsmServer:
 
 	def try_sending_messages(self, gsm_mod, gsm_info):
 		start = dt.now()
-		self.send_messages_from_db(gsm_mod, table='users', send_status=5,
-								   gsm_info=gsm_info)
+		self.send_messages_from_db(gsm_mod, table = 'users', send_status = 5,
+		gsm_info = gsm_info)
 
 	def run_server(self, gsm_mod, gsm_info, table='users'):
 		minute_of_last_alert = dt.now().minute
@@ -48,25 +46,24 @@ class GsmServer:
 		lastAlertMsgSent = ''
 		logruntimeflag = True
 		checkIfActive = True
-		print(">> GSM Server Active:", gsm_info['name'])
-		print(">> CSQ:", gsm_mod.getCSQ())
-		print(">> Initialization duration:",
-			  (time.time() - start_time), " seconds")
+		print('**', gsm_info['name'], ' GSM server active**')
+		print("CSQ:", gsm_mod.getCSQ())
+		print("--- ",(time.time() - start_time)," seconds ---")
 		while True:
 			m = gsm_mod.countSMS()
 			if m > 0:
 				allmsgs = gsm_mod.getAllSMS()
 				try:
 					db.write_inbox(allmsgs, gsm_info)
-					print(">> Writing SMS to Database...")
+					print("Writing to DB...")
 				except KeyboardInterrupt:
 					print(">> Error: May be an empty line.. skipping message storing")
 
 				gsm_mod.deleteSMS(gsm_info["module"])
 
 				print(dt.today().strftime(
-					">> Server active as of: %A, %B %d, %Y, %X"))
-				print(">> CSQ:", gsm_mod.getCSQ())
+				"\n Server active as of %A, %B %d, %Y, %X"))
+				print("CSQ:", gsm_mod.getCSQ())
 				self.try_sending_messages(gsm_mod, gsm_info)
 			elif m == 0:
 				self.try_sending_messages(gsm_mod, gsm_info)
@@ -74,85 +71,82 @@ class GsmServer:
 				if (today.minute % 10 == 0):
 					if checkIfActive:
 						print(dt.today().strftime(
-							">> Server active as of: %A, %B %d, %Y, %X"))
-						print(">> CSQ:", gsm_mod.getCSQ())
+						"\n Server active as of %A, %B %d, %Y, %X"))
+						print("CSQ:", gsm_mod.getCSQ())
 					checkIfActive = False
 				else:
 					checkIfActive = True
 			elif m == -1:
 				serverstate = 'inactive'
-				raise modem.ResetException(">> GSM Module inactive")
+				raise modem.ResetException("GSM MODULE MAYBE INACTIVE")
 			elif m == -2:
 				raise modem.ResetException(
-					">> Error in parsing mesages: No data returned by GSM")
+					"Error in parsing mesages: No data returned by GSM")
 			else:
 				raise modem.ResetException(
-					">> Error in parsing mesages: Error unknown")
+					"Error in parsing mesages: Error unknown")
 
-	def send_messages_from_db(self, gsm=None, table='users', send_status=5,
-							  gsm_info=None):
+	def send_messages_from_db(self, gsm = None, table = 'users', send_status = 5 , 
+		gsm_info = None):
 		if gsm == None:
-			raise ValueError(">> No GSM Instance defined")
+			raise ValueError("No gsm instance defined")
 
-		allmsgs = db.get_all_outbox_sms_from_db(table, send_status,
-												gsm_info["id"])
-
-		mobile_container = []
-		msglist = []
-		error_stat_list = []
+		allmsgs = db.get_all_outbox_sms_from_db(table, send_status, 
+			gsm_info["id"])
 
 		if len(allmsgs) <= 0:
+			print(">> No pending SMS...")
 			return
 
 		for msg in allmsgs:
-			table_mobile = db.getAllUsersMobile(msg[1], mobile_id_flag=True)
-			inv_table_mobile = {mobile_id: sim_num for (mobile_id, sim_num,
-														gsm_id) in table_mobile}
-			mobile_container.append(inv_table_mobile)
-			
-		today = dt.today().strftime("%Y-%m-%d %H:%M:%S")
+			table_mobile = db.getAllUsersMobile(msg[1], mobile_id_flag = True) # Refactor this one
+			inv_table_mobile =  {mobile_id:sim_num for (mobile_id, sim_num, 
+			gsm_id) in table_mobile}
 
-		for inv_mobile in mobile_container:
-			for stat_id, mobile_id, outbox_id, gsm_id, sms_msg in allmsgs:
-				try:
-					smsItem = modem.GsmSms(
-						stat_id, inv_mobile[mobile_id], sms_msg, '')
-					msglist.append([smsItem, gsm_id, outbox_id, mobile_id])
-				except KeyError as ke:
-					pass
-					# continue
-					
-		# if len(error_stat_list) > 0:
-		# 	print(">> Ignoring invalid messages...")
-		# 	db.updateSentStatus(table, error_stat_list)
-		# 	print("done")
+		msglist = []
+		error_stat_list = []
+		today = dt.today().strftime("%Y-%m-%d %H:%M:%S")
+		for stat_id, mobile_id, outbox_id, gsm_id, sms_msg in allmsgs:
+			try:
+				smsItem = modem.GsmSms(stat_id, inv_table_mobile[mobile_id], sms_msg,'')
+				msglist.append([smsItem, gsm_id, outbox_id, mobile_id])
+
+			except KeyError as ke:
+				print(ke)
+				print(">> Unknown mobile_id:", mobile_id)
+				error_stat_list.append((stat_id,-1,today,gsm_id,outbox_id,mobile_id))
+			
+				continue
+
+		if len(error_stat_list) > 0:
+			print(">> Ignoring invalid messages...", smstables.set_send_status(table, error_stat_list))
+			print("done")
 
 		if len(msglist) == 0:
 			print(">> No valid message to send")
 			return
-
+			
 		allmsgs = msglist
 		status_list = []
 
 		for msg in allmsgs:
 			try:
-				num_prefix = re.match(
-					"^ *((0)|(63))9\d\d", msg[0].simnum).group()
+				num_prefix = re.match("^ *((0)|(63))9\d\d",msg[0].simnum).group()
 				num_prefix = num_prefix.strip()
 				ret = gsm.sendSMS(msg[0].data, msg[0].simnum.strip())
 
 				today = dt.today().strftime("%Y-%m-%d %H:%M:%S")
 				if ret:
 					send_stat = 1
-					stat = msg[0].num, 1, today, msg[1], msg[2], msg[3]
+					stat = msg[0].num,1,today,msg[1],msg[2],msg[3]
 				else:
-					stat = msg[0].num, 5, today, msg[1], msg[2], msg[3]
+					stat = msg[0].num,5,today,msg[1],msg[2],msg[3]
 
 				status_list.append(stat)
 			except Exception as e:
-				print('>> Error:', e)
+				print('Exeception here')
+				print('Error:', e)
 		db.updateSentStatus(table, status_list)
-
 
 if __name__ == "__main__":
 	start_time = time.time()
@@ -178,7 +172,6 @@ if __name__ == "__main__":
 	gsm_info["baudrate"] = int(config['GSM_DEFAULT_SETTINGS']['BAUDRATE'])
 	gsm_info["port"] = gsm_modules[args.gsm_id]['port']
 	gsm_info["name"] = gsm_modules[args.gsm_id]['gsm_name']
-	gsm_info["module"] = gsm_modules[args.gsm_id]['module_type']
 
 	try:
 		initialize_gsm_modules = modem.GsmModem(gsm_info['port'], gsm_info["baudrate"],
@@ -196,6 +189,6 @@ if __name__ == "__main__":
 	try:
 		initialize_gsm.run_server(initialize_gsm_modules, gsm_info, 'users')
 	except modem.ResetException:
-		print(">> Resetting system because of GSM failure")
+		print("> Resetting system because of GSM failure")
 		gsm.reset()
 		sys.exit()
--- src/experimental_scripts/ws/users_scripts/db_lib.py
+++ src/experimental_scripts/ws/users_scripts/db_lib.py
@@ -4,6 +4,7 @@ import configparser
 from datetime import datetime as dt
 from datetime import timedelta as td
 from pprint import pprint
+import sys
 
 class DatabaseCredentials:
 	def __new__(self):
@@ -12,9 +13,11 @@ class DatabaseCredentials:
 		config["CBEWSL_DB_CREDENTIALS"]
 		return config
 
+
 class DatabaseConnection:
 
 	db_cred = None
+
 	def __init__(self):
 		self.db_cred = DatabaseCredentials()
 
@@ -24,18 +27,16 @@ class DatabaseConnection:
 
 		while True:
 			try:
-				db, cur = self.dbConnect(self.db_cred['CBEWSL_DB_CREDENTIALS']['host'], 
-					self.db_cred['CBEWSL_DB_CREDENTIALS']['user'], 
-					self.db_cred['CBEWSL_DB_CREDENTIALS']['password'], 
+				db, cur = self.dbConnect(
 					self.db_cred['CBEWSL_DB_CREDENTIALS']['db_comms'])
 				query = ("select t1.stat_id,t1.mobile_id,t1.gsm_id,t1.outbox_id,"
-					"t2.sms_msg from "
-					"smsoutbox_%s_status as t1 "
-					"inner join (select * from smsoutbox_%s) as t2 "
-					"on t1.outbox_id = t2.outbox_id "
-					"where t1.send_status < %d "
-					"and t1.send_status >= 0 "
-					"and t1.gsm_id = %d ") % (table[:-1],table,send_status,gsm_id)
+						 "t2.sms_msg from "
+						 "smsoutbox_%s_status as t1 "
+						 "inner join (select * from smsoutbox_%s) as t2 "
+						 "on t1.outbox_id = t2.outbox_id "
+						 "where t1.send_status < %d "
+						 "and t1.send_status >= 0 "
+						 "and t1.gsm_id = %d ") % (table[:-1], table, send_status, gsm_id)
 
 				a = cur.execute(query)
 				out = []
@@ -48,20 +49,22 @@ class DatabaseConnection:
 				print('10.')
 				time.sleep(20)
 
-	def getAllLoggersMobile(self, host, simnum):
+	def getAllLoggersMobile(self, sim_num):
 		try:
-			db, cur = self.dbConnect(self.db_cred['CBEWSL_DB_CREDENTIALS']['host'], 
-				self.db_cred['CBEWSL_DB_CREDENTIALS']['user'], 
-				self.db_cred['CBEWSL_DB_CREDENTIALS']['password'], 
+			db, cur = self.dbConnect(
 				self.db_cred['CBEWSL_DB_CREDENTIALS']['db_comms'])
-			query = ("SELECT t1.mobile_id, t1.sim_num, t1.gsm_id " \
-				"FROM logger_mobile AS t1 " \
-				"LEFT OUTER JOIN logger_mobile AS t2 " \
-				"ON t1.sim_num = t2.sim_num " \
-				"AND (t1.date_activated < t2.date_activated " \
-				"OR (t1.date_activated = t2.date_activated " \
-				"AND t1.mobile_id < t2.mobile_id)) " \
-				"WHERE t2.sim_num IS NULL and t1.sim_num is not null")
+			query = ("SELECT * FROM (SELECT "
+						"t1.mobile_id, t1.sim_num, t1.gsm_id "
+						"FROM "
+						"logger_mobile AS t1 "
+						"LEFT OUTER JOIN "
+						"logger_mobile AS t2 ON t1.sim_num = t2.sim_num "
+						"AND (t1.date_activated < t2.date_activated "
+						"OR (t1.date_activated = t2.date_activated "
+						"AND t1.mobile_id < t2.mobile_id)) "
+						"WHERE "
+						"t2.sim_num IS NULL "
+						"AND t1.sim_num IS NOT NULL) as logger_mobile WHERE sim_num like '%"+sim_num[:10]+"%'")
 
 			a = cur.execute(query)
 			out = []
@@ -73,16 +76,15 @@ class DatabaseConnection:
 		except MySQLdb.OperationalError:
 			time.sleep(20)
 
-	def getAllUsersMobile(self, sim_num, mobile_id_flag = False):
+	def getAllUsersMobile(self, sim_num, mobile_id_flag=False):
 		try:
-			db, cur = self.dbConnect(self.db_cred['CBEWSL_DB_CREDENTIALS']['host'], 
-				self.db_cred['CBEWSL_DB_CREDENTIALS']['user'], 
-				self.db_cred['CBEWSL_DB_CREDENTIALS']['password'], 
+			db, cur = self.dbConnect(
 				self.db_cred['CBEWSL_DB_CREDENTIALS']['db_comms'])
 			if mobile_id_flag == False:
 				query = "select mobile_id, sim_num, gsm_id from user_mobile where sim_num like '%"+sim_num+"%'"
 			else:
-				query = "select mobile_id, sim_num, gsm_id from user_mobile where mobile_id = '"+str(sim_num)+"'"
+				query = "select mobile_id, sim_num, gsm_id from user_mobile where mobile_id = '" + \
+					str(sim_num)+"'"
 			a = cur.execute(query)
 			out = []
 			if a:
@@ -91,74 +93,80 @@ class DatabaseConnection:
 			return out
 
 		except MySQLdb.OperationalError as mysqle:
-			print("MySQLdb OP Error:",mysqle)
+			print("MySQLdb OP Error:", mysqle)
 			time.sleep(20)
 
-	def write_inbox(self, msglist='',gsm_info=''):
+	def write_inbox(self, msglist='', gsm_info=''):
 		if not msglist:
-			raise ValueError("No msglist definition") # UNCOMMENT THIS ONE AFTER
+			raise ValueError("No msglist definition")
 
 		if not gsm_info:
-			raise ValueError("No gsm_info definition") # UNCOMMENT THIS ONE AFTER
+			raise ValueError("No gsm_info definition")
 
 		ts_stored = dt.today().strftime("%Y-%m-%d %H:%M:%S")
 
 		gsm_id = gsm_info['id']
 
-		loggers_count = 0
-		users_count = 0
-
-		#query_loggers = ("insert into smsinbox_loggers (ts_sms, ts_stored, mobile_id, "
-		#	"sms_msg,read_status,gsm_id) values ")                                           // TODO: change implem
-		query_users = ("insert into smsinbox_users (ts_sms, ts_stored, mobile_id, "
-			"sms_msg,read_status,gsm_id) values ")
 
 		sms_id_ok = []
 		sms_id_unk = []
 		ts_sms = 0
-		ltr_mobile_id= 0
+		ltr_mobile_id = 0
 
-		for m in msglist:
-			# print m.simnum, m.data, m.dt, m.num
-			ts_sms = m.dt
-			sms_msg = m.data
+		for msg in msglist:
+			ts_sms = msg.dt
+			sms_msg = msg.data
 			read_status = 0
 
-			#logger_mobile_sim_nums = self.getAllLoggersMobile(host, m.simnum[:10]) // TODO: change implem
-			user_mobile_sim_nums = self.getAllUsersMobile(m.simnum[:10])
+			query_loggers = self.writeLoggerSMS(msg, gsm_id, ts_sms, ts_stored, sms_msg)
+			if (query_loggers == -1):
+				query_users = self.writeUserSMS(msg, gsm_id, ts_sms, ts_stored, sms_msg)
+		
 
-			user_mobile_sim_nums = {sim_num: mobile_id for (mobile_id, sim_num, 
-			gsm_id) in user_mobile_sim_nums}
+	def writeLoggerSMS(self, msg, gsm_id, ts_sms, ts_stored, sms_msg):
+		loggers_count = 0
+		query_loggers = ("insert into smsinbox_loggers (ts_sms, ts_stored, mobile_id, "
+			"sms_msg,read_status,gsm_id) values ")
+		logger_mobile_sim_nums = self.getAllLoggersMobile(msg.simnum[-10:])
+		if len(logger_mobile_sim_nums) != 0:
+			logger_mobile_sim_nums = {sim_num: mobile_id for (mobile_id, sim_num,
+									gsm_id) in logger_mobile_sim_nums}
+			if msg.simnum in logger_mobile_sim_nums.keys():
+				query_loggers += "('%s','%s',%d,'%s',%d,%d)," % (ts_sms, ts_stored,
+					logger_mobile_sim_nums[msg.simnum], sms_msg, 0, gsm_id)
+				ltr_mobile_id= logger_mobile_sim_nums[msg.simnum]
+				loggers_count += 1
 
-			# if m.simnum in logger_mobile_sim_nums.keys():
-			# 	query_loggers += "('%s','%s',%d,'%s',%d,%d)," % (ts_sms, ts_stored,
-			# 		logger_mobile_sim_nums[m.simnum], sms_msg, read_status, gsm_id)
-			# 	ltr_mobile_id= logger_mobile_sim_nums[m.simnum]
-			# 	loggers_count += 1
-			if m.simnum in user_mobile_sim_nums.keys():
-				query_users += "('%s','%s',%d,'%s',%d,%d)," % (ts_sms, ts_stored,
-					user_mobile_sim_nums[m.simnum], sms_msg, read_status, gsm_id)
+			query_loggers = query_loggers[:-1]
+			print(">> Data logger SMS...")
+			result = self.writeToDB(query=query_loggers)
+			return result
+		else:
+			return -1
+
+	def writeUserSMS(self, msg, gsm_id, ts_sms, ts_stored, sms_msg):
+		users_count = 0
+		query_users = ("insert into smsinbox_users (ts_sms, ts_stored, mobile_id, "
+		   "sms_msg,gsm_id) values ")
+		user_mobile_sim_nums = self.getAllUsersMobile(msg.simnum[:10])
+		if len(user_mobile_sim_nums) != 0:
+			user_mobile_sim_nums = {sim_num: mobile_id for (mobile_id, sim_num,
+									gsm_id) in user_mobile_sim_nums}
+			if msg.simnum in user_mobile_sim_nums.keys():
+				query_users += "('%s','%s',%d,'%s',%d)," % (ts_sms, ts_stored,
+						   user_mobile_sim_nums[msg.simnum], sms_msg, gsm_id)
 				users_count += 1
-			else:            
-				print('Unknown number', m.simnum)
-				sms_id_unk.append(m.num)
-				continue
+			
+			query_users = query_users[:-1]
+			print(">> Users SMS...")
+			result = self.writeToDB(query=query_users)
+			return result
+		else:
+			return -1
 
-			sms_id_ok.append(m.num)
-
-		# query_loggers = query_loggers[:-1]
-		query_users = query_users[:-1]
-		if len(sms_id_ok)>0:
-			#if loggers_count > 0:
-			#    dbio.write(query=query_loggers, host=sms_host, resource=resource)
-			if users_count > 0:
-				self.writeToDB(query=query_users, host=host)
-
-	def writeToDB(self, query, last_insert_id = False):
+	def writeToDB(self, query, last_insert_id=False):
 		ret_val = None
-		db, cur = self.dbConnect(self.db_cred['CBEWSL_DB_CREDENTIALS']['host'], 
-			self.db_cred['CBEWSL_DB_CREDENTIALS']['user'], 
-			self.db_cred['CBEWSL_DB_CREDENTIALS']['password'], 
+		db, cur = self.dbConnect(
 			self.db_cred['CBEWSL_DB_CREDENTIALS']['db_comms'])
 
 		try:
@@ -168,14 +176,16 @@ class DatabaseConnection:
 				b = cur.execute('select last_insert_id()')
 				b = cur.fetchall()
 				ret_val = b
+			else:
+				ret_val = a
 
 		except IndexError:
 			print("IndexError on ")
 			print(str(inspect.stack()[1][3]))
 		except (MySQLdb.Error, MySQLdb.Warning) as e:
-			print (">> MySQL error/warning: %s" % e)
-			print ("Last calls:") 
-			for i in range(1,6):
+			print(">> MySQL error/warning: %s" % e)
+			print("Last calls:")
+			for i in range(1, 6):
 				try:
 					print("%s," % str(inspect.stack()[i][3]),)
 				except IndexError:
@@ -188,9 +198,7 @@ class DatabaseConnection:
 
 	def readDB(self, query):
 		try:
-			db, cur = self.dbConnect(self.db_cred['CBEWSL_DB_CREDENTIALS']['host'], 
-				self.db_cred['CBEWSL_DB_CREDENTIALS']['user'], 
-				self.db_cred['CBEWSL_DB_CREDENTIALS']['password'], 
+			db, cur = self.dbConnect(
 				self.db_cred['CBEWSL_DB_CREDENTIALS']['db_comms'])
 			a = cur.execute(query)
 			out = []
@@ -200,25 +208,27 @@ class DatabaseConnection:
 			return out
 
 		except MySQLdb.OperationalError as mysqle:
-			print("MySQLdb OP Error:",mysqle)
+			print("MySQLdb OP Error:", mysqle)
 			time.sleep(20)
 
-	def dbConnect(self, host, user, password, schema):
+	def dbConnect(self, schema):
 		try:
-			db = MySQLdb.connect(host, user, password, schema)
+			db = MySQLdb.connect(self.db_cred['CBEWSL_DB_CREDENTIALS']['host'],
+								 self.db_cred['CBEWSL_DB_CREDENTIALS']['user'],
+								 self.db_cred['CBEWSL_DB_CREDENTIALS']['password'], schema)
 			cur = db.cursor()
 			return db, cur
 		except TypeError:
 			print('Error Connection Value')
 			return False
 		except MySQLdb.OperationalError as mysqle:
-			print("MySQL Operationial Error:",mysqle)
+			print("MySQL Operationial Error:", mysqle)
 			return False
 		except (MySQLdb.Error, MySQLdb.Warning) as e:
-			print("MySQL Error:",e)
+			print("MySQL Error:", e)
 			return False
 
-	def updateSentStatus(self, table='',status_list='',resource="sms_data"):
+	def updateSentStatus(self, table='', status_list='', resource="sms_data"):
 		if not table:
 			raise ValueError("No table definition")
 
@@ -226,22 +236,23 @@ class DatabaseConnection:
 			raise ValueError("No status list definition")
 
 		query = ("insert into smsoutbox_%s_status (stat_id,send_status,ts_sent,"
-			"outbox_id,gsm_id,mobile_id) values ") % (table[:-1])
+				 "outbox_id,gsm_id,mobile_id) values ") % (table[:-1])
 
-		for stat_id,send_status,ts_sent,outbox_id,gsm_id,mobile_id in status_list:
+		for stat_id, send_status, ts_sent, outbox_id, gsm_id, mobile_id in status_list:
 			query += "(%d,%d,'%s',%d,%d,%d)," % (stat_id, send_status, ts_sent,
-				outbox_id, gsm_id, mobile_id)
+												 outbox_id, gsm_id, mobile_id)
 
 		query = query[:-1]
 		query += (" on duplicate key update stat_id=values(stat_id), "
-			"send_status=send_status+values(send_status),ts_sent=values(ts_sent)")
-		
+				  "send_status=send_status+values(send_status),ts_sent=values(ts_sent)")
+
 		self.writeToDB(query=query, last_insert_id=False)
 
 	def getGsmInfo(self, gsm_id):
 		gsm_dict = {}
-		query = "SELECT * FROM comms_db.gsm_modules where gsm_id = '"+str(gsm_id)+"';"
-		gsm_info = self.readDB(query) # Refactor this
+		query = "SELECT * FROM comms_db.gsm_modules where gsm_id = '" + \
+			str(gsm_id)+"';"
+		gsm_info = self.readDB(query)  # Refactor this
 		for gsm_id, gsm_server_id, gsm_name, sim_num, network, port, pwr, rng, module_type in gsm_info:
 			gsm_dict['gsm_id'] = gsm_id
 			gsm_dict['gsm_server_id'] = gsm_server_id
