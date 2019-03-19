2d82ecd04927cb84db86e1d6c7f60ef29f31ed8d
ksdelacruz <kevindhaledelacruz@gmail.com>
2019-03-19 17:39:07 +0800

    - Changes on monitoring

--- src/experimental_scripts/truncate_tables.py
+++ src/experimental_scripts/truncate_tables.py
@@ -9,7 +9,9 @@ TRUNCATE TABLE monitoring_on_demand;
 TRUNCATE TABLE monitoring_earthquake;
 TRUNCATE TABLE monitoring_moms;
 TRUNCATE TABLE moms_instances;
+TRUNCATE TABLE monitoring_moms_releases;
 
 ALTER TABLE monitoring_event_alerts AUTO_INCREMENT = 1;
 ALTER TABLE monitoring_triggers_misc AUTO_INCREMENT = 1;
+ALTER TABLE monitoring_moms_releases AUTO_INCREMENT = 1;
 """
