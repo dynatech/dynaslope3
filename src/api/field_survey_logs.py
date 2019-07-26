535d9dae8ed45c72ed6d7e8de917ea031f309966
jhndvdgvr <david063095@gmail.com>
2019-07-26 11:14:54 +0800

    +added sending email

--- src/api/field_survey_logs.py
+++ src/api/field_survey_logs.py
@@ -3,9 +3,10 @@ Inbox Functions Controller File
 """
 import time
 # import pdfkit
+from fpdf import FPDF
 import smtplib
 import os
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, url_for, send_from_directory
 from werkzeug import secure_filename
 from connection import DB, SOCKETIO
 from email.mime.text import MIMEText
@@ -18,6 +19,9 @@ from src.models.field_survey_logs import (
 
 FIELD_SURVEY_LOGS_BLUEPRINT = Blueprint(
     "field_survey_logs_blueprint", __name__)
+
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 
 @FIELD_SURVEY_LOGS_BLUEPRINT.route("/field_survey/get_all_field_survey", methods=["GET"])
@@ -136,7 +140,6 @@ def delete_field_survey():
     if data is None:
         data = request.form
 
-
     status = None
     message = ""
 
@@ -167,72 +170,97 @@ def process_pdf_and_email(data):
     return ""
 
 
-def field_survey_to_pdf():
+def latest_field_survey():
+    query = FieldSurveyLog.query.order_by(
+        FieldSurveyLog.field_survey_id.desc()).first()
 
-    return ""
+    result = FieldSurveyLogSchema().dump(query).data
 
-
-def field_survey_data_via_email(data):
-
-    features = data["features"]
-    mat_characterization = data["mat_characterization"]
-    mechanism = data["mechanism"]
-    exposure = data["exposure"]
-    note = data["note"]
-    date = data["date"]
-
-    email = "dynaslopeswat@gmail.com"
-    password = "dynaslopeswat"
-    send_to_email = "david063095@gmail.com"
-    subject = "Field Survey : " + str(date)
-    message = """
-    Features :  Scrap A
-                    -6m high from the road
-                    -53 m long
-                Scrap B
-                    -6m high from the road
-                    -53 m long
-    Material characterization : highly weathered sandstone
-    Mechanism : deep-seated rotational or translational landslide
-    Exposure : at least 9 households are at risk in the event of slope failure
-    Note : Sensor needs maintenance
-    """
-    file_location = 'pdf/test.pdf'
-
-    msg = MIMEMultipart()
-    msg['From'] = email
-    msg['To'] = send_to_email
-    msg['Subject'] = subject
-
-    msg.attach(MIMEText(message, 'plain'))
-
-    # Setup the attachment
-    filename = os.path.basename(file_location)
-    attachment = open(file_location, "rb")
-    part = MIMEBase('application', 'octet-stream')
-    part.set_payload(attachment.read())
-    encoders.encode_base64(part)
-    part.add_header('Content-Disposition',
-                    "attachment; filename= %s" % filename)
-
-    # Attach the attachment to the MIMEMultipart object
-    msg.attach(part)
-
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(email, password)
-    text = msg.as_string()
-    server.sendmail(email, send_to_email, text)
-    server.quit()
-
-    return ""
+    return result
 
 
-# @FIELD_SURVEY_LOGS_BLUEPRINT.route("/field_survey/upload", methods=["GET", "POST"])
+@FIELD_SURVEY_LOGS_BLUEPRINT.route("/field_survey/send_email", methods=["GET", "POST"])
+def field_survey_data_via_email():
+    latest_data = latest_field_survey()
+    data = request.form
+    status = None
+    message = ""
+    try:
+        features = latest_data["features"]
+        mat_characterization = latest_data["mat_characterization"]
+        mechanism = latest_data["mechanism"]
+        exposure = latest_data["exposure"]
+        note = latest_data["note"]
+        date = data["date"]
+
+        email = "dynaslopeswat@gmail.com"
+        password = "dynaslopeswat"
+        send_to_email = data["email"]
+        subject = "Field Survey : " + str(date)
+
+        message = "<b>Features:</b> " + features + "<br>"
+        message += "<b>Material characterization:</b> " + mat_characterization + "<br>"
+        message += "<b>Mechanism:</b> " + mechanism + "<br>"
+        message += "<b>Exposure:</b> " + exposure + "<br>"
+        message += "<b>Note:</b> " + note + "<br>"
+        # file_location = 'test.pdf'
+
+        msg = MIMEMultipart()
+        msg['From'] = email
+        msg['To'] = send_to_email
+        msg['Subject'] = subject
+
+        msg.attach(MIMEText(message, 'html'))
+
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(email, password)
+        text = msg.as_string()
+        server.sendmail(email, send_to_email, text)
+        server.quit()
+
+        status = True
+        message = "Email sent successfully!"
+    except Exception as err:
+        print(err)
+        DB.session.rollback()
+        status = False
+        message = "Something went wrong, Please try again"
+
+    feedback = {
+        "status": status,
+        "message": message
+    }
+    return jsonify(feedback)
+
+
+@FIELD_SURVEY_LOGS_BLUEPRINT.route("/moms/upload", methods=["GET", "POST"])
 def file_upload_sample():
+    # if request.method == 'POST':
+    #     # check if the post request has the files part
+    #     if 'files[]' not in request.files:
+    #         flash('No file part')
+    #         return jsonify("no file")
+    #     files = request.files.getlist('files[]')
+    #     for file in files:
+    #         if file and allowed_file(file.filename):
+    #             filename = secure_filename(file.filename)
+    #             file.save(os.path.join("moms_images", filename))
+    #     # flash('File(s) successfully uploaded')
+    #     return jsonify("wqewqwe")
+
     if request.method == "POST":
-        file = request.files["file"]
-        file_name = secure_filename(file.filename)
-        file.save(os.path.join("pdf", file_name))
-    print("success upload")
-    return "file uploaded successfully"
+        print("herrrererer")
+        print(request.file_to_upload)
+        try:
+            if 'file' in request.files:
+                imageFile = request.files['file']
+                imageFile.save(os.path.join("moms_images", imageFile))
+        except Exception as e:
+            print(e)
+        return "ok"
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
