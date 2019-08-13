b0f37e8c7cdde4ab3eb531a4be7d19f29c2f2255
ksdelacruz <kevindhaledelacruz@gmail.com>
2019-08-13 11:19:16 +0800

    - edited rainfall api

--- src/api/rainfall.py
+++ src/api/rainfall.py
@@ -2,32 +2,40 @@
 """
 
 from flask import Blueprint, jsonify, request
-import os
-import time
-import analysis.rainfall.rainfall as rain
+from datetime import time
 from connection import DB, SOCKETIO
-from src.models.sensor_maintenance import (
-    SensorMaintenance, SensorMaintenanceSchema)
+import analysis.rainfall.rainfall as rainfall
 
 RAINFALL_BLUEPRINT = Blueprint(
     "rainfall_blueprint", __name__)
 
 
 @RAINFALL_BLUEPRINT.route("/rainfall/get_rainfall_data", methods=["GET", "POST"])
-def get_rainfall_data():
-    data = request.get_json()
-    print(data)
-    rainfall_data = rain.main(
-        data['site_code'], end=data['date'], print_plot=True, save_plot=False, days=3)
+@RAINFALL_BLUEPRINT.route("/rainfall/get_rainfall_data/<site_code>", methods=["GET", "POST"])
+@RAINFALL_BLUEPRINT.route("/rainfall/get_rainfall_data/<site_code>/<end_ts>", methods=["GET", "POST"])
+def get_rainfall_data(site_code=None, end_ts=None):
+    sc = site_code
+    ts = end_ts
+
+    if site_code is None:
+        data = request.get_json()
+        sc = data["site_code"]
+        ts = data["date"]
+
+    rainfall_data = rainfall.main(
+        sc, end=ts, print_plot=True, save_plot=False, days=3)
     return rainfall_data
 
 
-@RAINFALL_BLUEPRINT.route("/rainfall/get_rainfall_plot_data", methods=["GET", "POST"])
-def get_rainfall_plot_data():
-    current_date_time = time.strftime('%Y-%m-%d %H:%M:%S')
-    #current_date_time = "2018-07-10 15:30:00"
-    rainfall_data = rain.main(
-        site_code="umi", end=current_date_time, print_plot=True, save_plot=False, days=7)
+@RAINFALL_BLUEPRINT.route("/rainfall/get_rainfall_plot_data/<site_code>", methods=["GET", "POST"])
+@RAINFALL_BLUEPRINT.route("/rainfall/get_rainfall_plot_data/<site_code>/<end_ts>", methods=["GET", "POST"])
+def get_rainfall_plot_data(site_code, end_ts=None):
+    ts = end_ts
+    if end_ts is None:
+        ts = time.strftime("%Y-%m-%d %H:%M:%S")
+
+    rainfall_data = rainfall.main(
+        site_code=site_code, end=ts, print_plot=True, save_plot=False, days=7)
     final_data = []
 
     return rainfall_data
