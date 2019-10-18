db6daf6e1a9aa67f38a978adc10d0f6715d0deaa
John David Guevarra <david063095@gmail.com>
2019-10-18 14:58:23 +0800

    +uncommented functions

--- src/api/rainfall.py
+++ src/api/rainfall.py
@@ -4,38 +4,38 @@
 from flask import Blueprint, jsonify, request
 from datetime import time, datetime
 from connection import DB, SOCKETIO
-# import analysis.rainfall.rainfall as rainfall
+import analysis.rainfall.rainfall as rainfall
 
 RAINFALL_BLUEPRINT = Blueprint(
     "rainfall_blueprint", __name__)
 
 
-# @RAINFALL_BLUEPRINT.route("/rainfall/get_rainfall_data", methods=["GET", "POST"])
-# @RAINFALL_BLUEPRINT.route("/rainfall/get_rainfall_data/<site_code>", methods=["GET", "POST"])
-# @RAINFALL_BLUEPRINT.route("/rainfall/get_rainfall_data/<site_code>/<end_ts>", methods=["GET", "POST"])
-# def get_rainfall_data(site_code=None, end_ts=None):
-#     sc = site_code
-#     ts = end_ts
+@RAINFALL_BLUEPRINT.route("/rainfall/get_rainfall_data", methods=["GET", "POST"])
+@RAINFALL_BLUEPRINT.route("/rainfall/get_rainfall_data/<site_code>", methods=["GET", "POST"])
+@RAINFALL_BLUEPRINT.route("/rainfall/get_rainfall_data/<site_code>/<end_ts>", methods=["GET", "POST"])
+def get_rainfall_data(site_code=None, end_ts=None):
+    sc = site_code
+    ts = end_ts
 
-#     if site_code is None:
-#         data = request.get_json()
-#         sc = data["site_code"]
-#         ts = data["date"]
+    if site_code is None:
+        data = request.get_json()
+        sc = data["site_code"]
+        ts = data["date"]
 
-#     rainfall_data = rainfall.main(
-#         sc, end=ts, print_plot=True, save_plot=False, days=3)
-#     return rainfall_data
+    rainfall_data = rainfall.main(
+        sc, end=ts, print_plot=True, save_plot=False, days=3)
+    return rainfall_data
 
 
-# @RAINFALL_BLUEPRINT.route("/rainfall/get_rainfall_plot_data/<site_code>", methods=["GET", "POST"])
-# @RAINFALL_BLUEPRINT.route("/rainfall/get_rainfall_plot_data/<site_code>/<end_ts>", methods=["GET", "POST"])
-# def get_rainfall_plot_data(site_code, end_ts=None):
-#     ts = end_ts
-#     if end_ts is None:
-#         ts = datetime.now()
+@RAINFALL_BLUEPRINT.route("/rainfall/get_rainfall_plot_data/<site_code>", methods=["GET", "POST"])
+@RAINFALL_BLUEPRINT.route("/rainfall/get_rainfall_plot_data/<site_code>/<end_ts>", methods=["GET", "POST"])
+def get_rainfall_plot_data(site_code, end_ts=None):
+    ts = end_ts
+    if end_ts is None:
+        ts = datetime.now()
 
-#     rainfall_data = rainfall.main(
-#         site_code=site_code, end=ts, print_plot=True, save_plot=False, days=7)
-#     final_data = []
+    rainfall_data = rainfall.main(
+        site_code=site_code, end=ts, print_plot=True, save_plot=False, days=7)
+    final_data = []
 
-#     return rainfall_data
+    return rainfall_data
