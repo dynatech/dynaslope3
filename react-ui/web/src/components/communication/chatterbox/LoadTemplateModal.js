4691040772cc0ac7bc1ff567590ec1aa795875fb
John David Guevarra <david063095@gmail.com>
2020-01-29 17:47:03 +0800

    - refactor implementaion of rain information message generation

--- react-ui/web/src/components/communication/chatterbox/LoadTemplateModal.js
+++ react-ui/web/src/components/communication/chatterbox/LoadTemplateModal.js
@@ -116,14 +116,15 @@ function LoadTemplateModal (props) {
     const getRainInformationFunction = () => {
         const site_details = getSiteCodes(site_list);
         const is_express = express;
+        const as_of = moment(date_time).format("LLL");
         const final_data = {
             is_express,
             site_details,
-            date_time
+            date_time,
+            as_of
         };
         getRainInformation(final_data, data => {
             const { status, message, ewi } = data;
-            const as_of = moment(date_time).format("LLL");
             const ewi_message = ewi.replace("<as_of>", as_of);
             setComposedMessage(ewi_message);
             if (status === true) {
