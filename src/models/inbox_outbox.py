247d18c52d9ea1ad4e3d514b692b9f0c875cac3b
ksdelacruz <kevindhaledelacruz@gmail.com>
2020-06-03 21:54:41 +0800

    - changed default datetime

--- src/models/inbox_outbox.py
+++ src/models/inbox_outbox.py
@@ -19,8 +19,8 @@ class SmsInboxUsers(DB.Model):
     __table_args__ = {"schema": "mia_comms_db_3"}
 
     inbox_id = DB.Column(DB.Integer, primary_key=True)
-    ts_sms = DB.Column(DB.DateTime, default=datetime.utcnow)
-    ts_stored = DB.Column(DB.DateTime, default=datetime.utcnow)
+    ts_sms = DB.Column(DB.DateTime, default=datetime.now)
+    ts_stored = DB.Column(DB.DateTime, default=datetime.now)
     mobile_id = DB.Column(
         DB.Integer, DB.ForeignKey("mia_comms_db_3.user_mobiles.mobile_id"))
     sms_msg = DB.Column(DB.String(1000))
@@ -47,8 +47,8 @@ class SmsInboxUsers2(DB.Model):
     __table_args__ = {"schema": "mia_comms_db"}
 
     inbox_id = DB.Column(DB.Integer, primary_key=True)
-    ts_sms = DB.Column(DB.DateTime, default=datetime.utcnow)
-    ts_stored = DB.Column(DB.DateTime, default=datetime.utcnow)
+    ts_sms = DB.Column(DB.DateTime, default=datetime.now)
+    ts_stored = DB.Column(DB.DateTime, default=datetime.now)
     mobile_id = DB.Column(DB.Integer)
     sms_msg = DB.Column(DB.String(1000))
     read_status = DB.Column(DB.Integer, default=0)
@@ -93,7 +93,7 @@ class SmsInboxUserTags(DB.Model):
         "mia_comms_db_3.sms_tags.tag_id"))
     user_id = DB.Column(
         DB.Integer, DB.ForeignKey("mia_commons_db.users.user_id"))
-    ts = DB.Column(DB.DateTime, default=datetime.utcnow)
+    ts = DB.Column(DB.DateTime, default=datetime.now)
 
     tag = DB.relationship("SmsTags",
                           backref=DB.backref(
@@ -114,7 +114,7 @@ class SmsOutboxUsers(DB.Model):
     __table_args__ = {"schema": "mia_comms_db_3"}
 
     outbox_id = DB.Column(DB.Integer, primary_key=True)
-    ts_written = DB.Column(DB.DateTime, default=datetime.utcnow)
+    ts_written = DB.Column(DB.DateTime, default=datetime.now)
     source = DB.Column(DB.String(45))
     sms_msg = DB.Column(DB.String(1000))
 
@@ -139,7 +139,7 @@ class SmsOutboxUsers2(DB.Model):
     __table_args__ = {"schema": "mia_comms_db"}
 
     outbox_id = DB.Column(DB.Integer, primary_key=True)
-    ts_written = DB.Column(DB.DateTime, default=datetime.utcnow)
+    ts_written = DB.Column(DB.DateTime, default=datetime.now)
     source = DB.Column(DB.String(45))
     sms_msg = DB.Column(DB.String(1000))
 
@@ -205,7 +205,7 @@ class SmsOutboxUserTags(DB.Model):
         "mia_comms_db_3.sms_tags.tag_id"))
     user_id = DB.Column(
         DB.Integer, DB.ForeignKey("mia_commons_db.users.user_id"))
-    ts = DB.Column(DB.DateTime, default=datetime.utcnow)
+    ts = DB.Column(DB.DateTime, default=datetime.now)
 
     tag = DB.relationship("SmsTags",
                           backref=DB.backref(
