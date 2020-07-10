e3eb4d6cc7ad0a4671efc4a395bc60de36ae0a62
harleosorio <harle.osorio2@gmail.com>
2020-07-11 07:31:57 +0800

    AAAA

--- react-ui/web/src/config.js
+++ react-ui/web/src/config.js
@@ -1,5 +1,5 @@
-// export const host = "http://192.168.150.110:5000";
-export const host = "http://localhost:5000";
-// export const react_host = "http://192.168.150.110";
-export const react_host = "http://localhost:3000";
+export const host = "http://192.168.150.110:5000";
+// export const host = "http://localhost:5000";
+export const react_host = "http://192.168.150.110";
+// export const react_host = "http://localhost:3000";
 export const access_refresh_interval = 1000 * 60 * 25; // 25 minutes
