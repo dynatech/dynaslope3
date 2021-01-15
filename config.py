83f46704070b64c4753c9fae6244b65991328e04
ksdelacruz <kevindhaledelacruz@gmail.com>
2021-01-15 13:26:18 +0800

    - added additional config for gen alert writing to db (for sandbox)

--- config.py
+++ config.py
@@ -67,7 +67,8 @@ APP_CONFIG = {
     "logs_path": f"{ROOT_PATH}/logs",
     # "url": "http://192.168.150.167:3000",
     "url": "http://192.168.150.110",
-    "is_live_mode": True,
+    "is_live_mode": True,  # used mainly in comms (e.g. bulletin)
+    "save_generated_alert_to_db": True,
     "smtp_server": "smtp.gmail.com",
     "port": 587
 }
