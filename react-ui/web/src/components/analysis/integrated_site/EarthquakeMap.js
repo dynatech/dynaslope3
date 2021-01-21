b2c0370c5421881c51993eb0f2b64f89dabb720b
ksdelacruz <kevindhaledelacruz@gmail.com>
2021-01-21 13:56:18 +0800

    - updated Mapbox API

--- react-ui/web/src/components/analysis/integrated_site/EarthquakeMap.js
+++ react-ui/web/src/components/analysis/integrated_site/EarthquakeMap.js
@@ -66,7 +66,7 @@ function EarthquakeMap (props) {
             <TileLayer
                 attribution='&copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
                 id="mapbox.streets"
-                url="https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw"
+                url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw"
             />
             {
                 eqEvents.map((event, i) => {
